56d8f37bd3b2f9312412f02ad69bc566c6cde4ea
eemiljka <eemiljka@metropolia.fi>
2024-09-06 19:39:55 +0300

    base done, trying to add username function

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+});
+
+io.on("connection", (socket) => {
+  socket.broadcast.emit("hi");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3000, () => {
